a23afc3ce02aea1595f1f80c91ad4b66303793e9
Robert Allen <rallen_prsch911@yahoo.com>
2015-02-11 21:57:22 -0600

    Adding Detail Activity to show weather details for a certain day once it is clicked on in the list.

--- app/src/main/java/com/haxaw/sunshine/DetailActivity.java
+++ app/src/main/java/com/haxaw/sunshine/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.haxaw.sunshine;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 //import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -9,6 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 //import android.os.Build;
 
 
@@ -59,7 +61,15 @@ public class DetailActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            Intent intent = getActivity().getIntent();
+
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            String forecastStr = intent.getStringExtra(intent.EXTRA_TEXT);
+
+            TextView textView = (TextView)rootView.findViewById(R.id.detail_text);
+            textView.setText(forecastStr);
+
             return rootView;
         }
     }
