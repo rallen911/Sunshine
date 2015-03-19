11d88331a87a9aaded7a544227fba5ebd409cf01
Robert Allen <rallen_prsch911@yahoo.com>
2015-03-19 00:31:05 -0500

    Lesson 5 - Completed!

--- app/src/main/java/com/haxaw/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/haxaw/sunshine/ForecastAdapter.java
@@ -19,20 +19,30 @@ public class ForecastAdapter extends CursorAdapter
     private final int VIEW_TYPE_FUTURE_DAY = 1;
     private static final int VIEW_TYPE_COUNT = 2;
 
+    // Flag to determine if we want to use a separate view for "today".
+    private boolean mUseTodayLayout = true;
+
+    /**
+     * Cache of the children views for a forecast list item.
+     */
+    public static class ViewHolder {
+        public final ImageView iconView;
+        public final TextView dateView;
+        public final TextView descriptionView;
+        public final TextView highTempView;
+        public final TextView lowTempView;
+
+        public ViewHolder(View view) {
+            iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+            dateView = (TextView) view.findViewById( R.id.list_item_date_textview );
+            descriptionView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+            highTempView = (TextView) view.findViewById(R.id.list_item_high_textview);
+            lowTempView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        }
+    }
+
     public ForecastAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
-    }
-
-    @Override
-    public int getItemViewType( int position )
-    {
-        return ( position == 0 )? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
-    }
-
-    @Override
-    public int getViewTypeCount()
-    {
-        return VIEW_TYPE_COUNT;
     }
 
     /*
@@ -113,22 +123,19 @@ public class ForecastAdapter extends CursorAdapter
         viewHolder.lowTempView.setText( Utility.formatTemperature( context, low, isMetric ));
     }
 
-    /**
-     * Cache of the children views for a forecast list item.
-     */
-    public static class ViewHolder {
-        public final ImageView iconView;
-        public final TextView dateView;
-        public final TextView descriptionView;
-        public final TextView highTempView;
-        public final TextView lowTempView;
+    public void setUseTodayLayout(boolean useTodayLayout) {
+        mUseTodayLayout = useTodayLayout;
+    }
 
-        public ViewHolder(View view) {
-            iconView = (ImageView) view.findViewById(R.id.list_item_icon);
-            dateView = (TextView) view.findViewById( R.id.list_item_date_textview );
-            descriptionView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
-            highTempView = (TextView) view.findViewById(R.id.list_item_high_textview);
-            lowTempView = (TextView) view.findViewById(R.id.list_item_low_textview);
-        }
+    @Override
+    public int getItemViewType( int position )
+    {
+        return ( position == 0 && mUseTodayLayout )? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount()
+    {
+        return VIEW_TYPE_COUNT;
     }
 }
